d76f19a7ad98bb1060706858a7bf361f00e555c8
Pablo Rosa <pabloterosa@gmail.com>
2025-05-28 14:05:25 -0300

    creacion del carrucel de imagenes

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,19 @@
+import PhotoCarousel from "@/components/slice/page";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-
+    <div className="bg-background">
+      <PhotoCarousel />
+      {/* Aquí puedes agregar cualquier otro contenido que quieras en la página principal */}
+      <section className="container mx-auto py-8">
+        <h1 className="text-3xl font-bold text-center">
+          Bienvenidos a Bara Creativa
+        </h1>
+        <p className="text-center mt-4">
+          Edtech hondureña que ofrece cursos e-learning y presenciales de alto
+          impacto.
+        </p>
+      </section>
     </div>
   );
 }
