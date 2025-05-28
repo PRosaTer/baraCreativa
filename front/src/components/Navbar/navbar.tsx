8362bd7a20d922959c44b544837f3a0b178ba7c7
Ivan Ameri <dark.ranked@gmail.com>
2025-05-28 09:52:43 -0300

    Cambios para navbar

---
+++ front/src/components/Navbar/navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="w-full h-[108px] bg-primary mx-auto"></nav>
+  );
+}
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,6 +1,17 @@
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+   extend: {
+  colors: {
+    primary: 'var(--color-primary)',
+    background: 'var(--background)',
+    foreground: 'var(--foreground)',
   },
+},
+
+  },
+  plugins: [],
 };
