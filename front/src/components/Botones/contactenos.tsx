219d25bf445d0035372d1f40e0bc2a3a28ebe6eb
Ivan Ameri <dark.ranked@gmail.com>
2025-05-28 11:07:44 -0300

    Creacion de todos los botones, creacion de barra de busqueda, modificacion en el logo y responsivo

---
+++ front/src/components/Botones/contactenos.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react';
+
+export default function ContactenosButton() {
+  return (
+<button className="min-w-[88px] sm:min-w-[72px] md:min-w-[80px] lg:min-w-[88px] 
+                    h-[53px] sm:h-[45px] md:h-[50px] lg:h-[53px] 
+                    ml-[54px] sm:ml-[40px] md:ml-[60px] lg:ml-[54px] 
+                    flex items-center justify-center text-foreground font-medium px-4 
+                    rounded-[40px] bg-[#F2E4E4] relative overflow-hidden group">
+  <span className="absolute inset-0 rounded-[40px] border-4 border-transparent bg-gradient-to-r from-red-600 via-yellow-400 to-white bg-[length:300%_300%] opacity-0 group-hover:opacity-100 animate-gradient-move pointer-events-none"></span>
+  <span className="relative z-10">Contáctenos</span>
+</button>
+
+  );
+}
---
+++ front/src/components/logo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function Logo() {
+  return (
+    <div className="flex items-center h-full ml-[60px]">
+      <Image
+        src="/logo-bc.png"
+        alt="Bara Creativa Logo"
+        width={66}
+        height={53}
+        priority
+      />
+    </div>
+  );
+}
