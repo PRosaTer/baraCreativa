90ea448b2e2675736456d86b1e8fb5b73429f368
Pablo Rosa <pabloterosa@gmail.com>
2025-05-30 12:48:57 -0300

    reestructurando los contenedores

--- front/src/components/DynamicContenedor2/page.tsx
+++ front/src/components/DynamicContenedor2/page.tsx
@@ -2,15 +2,23 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import Contenedor_1 from "@/components/contenedor_1/page";
 
-// Importamos dinámicamente Contenedor_2
 const Contenedor_2 = dynamic(() => import("@/components/contenedor_2/page"), {
   loading: () => <p>Cargando contenido pesado...</p>,
-  ssr: false, // Ahora esto es válido porque estamos en un Client Component
+  ssr: false,
 });
 
 const DynamicContenedor2: React.FC = () => {
-  return <Contenedor_2 />;
+  return (
+    <Contenedor_1 bgColor="bg-gray-300">
+      <div className="text-center p-4">
+        <h2 className="text-2xl font-bold mb-4">Capacitaciones Pesadas</h2>
+        <p>Contenido pesado cargado dinámicamente.</p>
+        <Contenedor_2 />
+      </div>
+    </Contenedor_1>
+  );
 };
 
 export default DynamicContenedor2;
