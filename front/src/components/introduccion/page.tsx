c7a9d1e7ca25aadb8dbbdcc4bdae6df5385e9b74
Pablo Rosa <pabloterosa@gmail.com>
2025-07-25 13:15:28 -0300

    actualizando lo que dice en la introduccion de la pagina principal

--- front/src/components/introduccion/page.tsx
+++ front/src/components/introduccion/page.tsx
@@ -19,16 +19,16 @@ const Introduction = () => {
         />
         <div className="flex flex-col justify-center max-w-md">
           <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">
-            Si puedes pensarlo, <br />
+            Si puedes imaginarlo, <br />
             <TypeAnimation
               sequence={[
-                "puedes programarlo",
+                "puedes aprenderlo",
                 1000,
-                "puedes optimizarlo",
+                "puedes enseñarlo",
                 1000,
-                "puedes implementarlo",
+                "puedes crearlo con nosotros",
                 1000,
-                "puedes desarrollarlo",
+                "puedes transformarlo en conocimiento",
                 1000,
               ]}
               wrapper="span"
@@ -39,11 +39,14 @@ const Introduction = () => {
           </h1>
 
           <p className="mx-auto mb-2 text-xl md:text-xl md:mx-0 md:mb-8">
-            Hola! Un placer conocerte, mi nombre es pablo, Soy desarrollador web
-            FullStack. Tengo experiencia en tecnologias como React. Nextjs.
-            Node.js. Express. Javascript. Typescript, Tailwind y Bootstrap.
-            Busco constantemente mejorar mis habilidades y estar a la vanguardia
-            de las tendencias tecnológicas.
+            ¡Hola! Un placer conocerte, mi nombre es Víctor y soy el fundador de
+            Bara Creativa, una academia digital enfocada en transformar la
+            educación a través de la tecnología. Combinamos herramientas como la
+            gamificación, la inteligencia artificial y el diseño interactivo
+            para crear experiencias de aprendizaje innovadoras, dinámicas y
+            accesibles. Buscamos constantemente mejorar nuestras metodologías
+            para mantenernos a la vanguardia y brindar una educación más
+            atractiva, inclusiva y efectiva para todos.
           </p>
 
           <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
