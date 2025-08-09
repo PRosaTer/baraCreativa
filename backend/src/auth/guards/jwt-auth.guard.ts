6114ffdffe583a7d5f376f0268aaddc785a05562
Ivan Ameri <dark.ranked@gmail.com>
2025-08-09 08:27:24 -0300

    Modificacion de auth en back

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext, Logger } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  private readonly logger = new Logger(JwtAuthGuard.name);
+
+  canActivate(context: ExecutionContext) {
+    this.logger.log('JwtAuthGuard: canActivate se ha llamado.');
+    const request = context.switchToHttp().getRequest();
+    this.logger.log(`JwtAuthGuard: Cookies en la petición: ${JSON.stringify(request.cookies)}`);
+    return super.canActivate(context);
+  }
+}
