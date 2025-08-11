b515cc309ae8ede3442a59a6898a5fdd10361811
Ivan Ameri <dark.ranked@gmail.com>
2025-08-11 15:56:20 -0300

    Cambios en auth.controller

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -62,7 +62,7 @@ export class AuthController {
 
     res.cookie('jwt', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true, 
       sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
       path: '/',
