ca44192a65c5eeb5901d220f0661068532828c9e
Ivan Ameri <dark.ranked@gmail.com>
2025-08-11 19:19:26 -0300

    cambios en EstadoUsuario

--- front/src/components/usuarios/EstadoUsuario.tsx
+++ front/src/components/usuarios/EstadoUsuario.tsx
@@ -1,15 +1,11 @@
-'use client';
+"use client";
 
 import React, { useEffect, useState } from 'react';
 import { useSocket } from '@/app/context/SocketProvider';
+import { Usuario } from '@/app/types/auth'; 
 
 interface Props {
   usuarioId: number;
-}
-
-interface EstadoActualizadoPayload {
-  usuarioId: number;
-  estaConectado: boolean;
 }
 
 export default function EstadoUsuario({ usuarioId }: Props) {
@@ -17,20 +13,33 @@ export default function EstadoUsuario({ usuarioId }: Props) {
   const [conectado, setConectado] = useState(false);
 
   useEffect(() => {
-    if (!socket) return;
+    if (!socket) {
+        console.warn('Socket no disponible en EstadoUsuario.');
+        return;
+    }
 
-    const actualizarEstado = (data: EstadoActualizadoPayload) => {
-      if (data.usuarioId === usuarioId) {
-        setConectado(data.estaConectado);
+   
+    const actualizarEstado = (usuarios: Usuario[]) => {
+      const usuarioActualizado = usuarios.find(u => u.id === usuarioId);
+      
+
+      if (usuarioActualizado) {
+        setConectado(usuarioActualizado.estaConectado);
       }
     };
 
-    socket.on('estadoActualizado', actualizarEstado);
 
+    socket.on('usuariosActualizados', actualizarEstado);
+
+
+    if (socket.connected) {
+        socket.emit('getUsuarios');
+    }
 
     return () => {
-      socket.off('estadoActualizado', actualizarEstado);
+      socket.off('usuariosActualizados', actualizarEstado);
     };
+    
   }, [usuarioId, socket]);
 
   return (
