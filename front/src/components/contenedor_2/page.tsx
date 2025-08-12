08f462d7de7345ee7d07548f7d6c2b28a57455e1
Ivan Ameri <dark.ranked@gmail.com>
2025-08-12 15:43:31 -0300

    agrego responsive nuevamente

--- front/src/components/contenedor_2/page.tsx
+++ front/src/components/contenedor_2/page.tsx
@@ -8,9 +8,14 @@ const HeavyComponent = () => {
   return (
     <div className="w-full max-w-[667px] h-[750px] lg:h-[592px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-4">
       <VideoSlider />
-      <div className="mt-auto h-[300px]"> 
-        <ContenedorDeTestimonios />
-      </div>
+     <div className="
+  mt-auto 
+  h-[200px]          /* altura base */
+  md:h-[300px]       /* altura +100px en pantallas md (768px+) */
+  lg:h-[250px]       /* altura intermedia en pantallas grandes */
+">
+  <ContenedorDeTestimonios />
+</div>
     </div>
   );
 };
