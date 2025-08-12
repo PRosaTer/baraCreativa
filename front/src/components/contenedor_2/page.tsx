45e68d57eddee00a625e6091d52d6b248425cdf7
Ivan Ameri <dark.ranked@gmail.com>
2025-08-12 16:11:38 -0300

    ajuste final

--- front/src/components/contenedor_2/page.tsx
+++ front/src/components/contenedor_2/page.tsx
@@ -7,14 +7,14 @@ import ContenedorDeTestimonios from "./contenedorDeTestimonios";
 const HeavyComponent = () => {
   return (
     <div
-      className="w-full max-w-[667px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-4 h-[850px]"
+      className="w-full max-w-[667px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-4 h-[800px]"
     >
-      {/* VideoSlider con altura justa */}
+
       <div className="w-full h-auto">
         <VideoSlider />
       </div>
 
-      {/* ContenedorDeTestimonios con altura fija para que quepa en el padre */}
+
       <div className="mt-auto w-full h-[300px]">
         <ContenedorDeTestimonios />
       </div>
