8e2fef3a9638b829d8ad842c69b8046c53188a2f
Ivan Ameri <dark.ranked@gmail.com>
2025-08-12 09:38:58 -0300

    Alineo componentes en Contenedor_2

--- front/src/components/contenedor_2/page.tsx
+++ front/src/components/contenedor_2/page.tsx
@@ -4,14 +4,19 @@ import React from "react";
 import VideoSlider from "../contenedor_2/VideoSlider";
 import ContenedorDeTestimonios from "./contenedorDeTestimonios";
 
-
 const HeavyComponent = () => {
   return (
-   <div className="w-[667px] h-[592px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-1">
-  <VideoSlider />
-  <ContenedorDeTestimonios />
-</div>
-
+    <div
+      className="
+        w-full max-w-[667px] h-auto lg:h-[592px]
+        bg-gray-200 rounded-lg p-4
+        flex flex-col lg:flex-row
+        items-start justify-start gap-4
+      "
+    >
+      <VideoSlider />
+      <ContenedorDeTestimonios />
+    </div>
   );
 };
 
