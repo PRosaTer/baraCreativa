8fd5a8cf3c3066703752a4f1bec19e88265172de
Ivan Ameri <dark.ranked@gmail.com>
2025-08-12 10:32:14 -0300

    Modifico tamaño responsivo del contenedor de testimoniios

--- front/src/components/contenedor_2/page.tsx
+++ front/src/components/contenedor_2/page.tsx
@@ -4,14 +4,12 @@ import React from "react";
 import VideoSlider from "../contenedor_2/VideoSlider";
 import ContenedorDeTestimonios from "./contenedorDeTestimonios";
 
-
 const HeavyComponent = () => {
   return (
-   <div className="w-[667px] h-[592px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-1">
-  <VideoSlider />
-  <ContenedorDeTestimonios />
-</div>
-
+    <div className="w-full max-w-[667px] h-[592px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-1">
+      <VideoSlider />
+      <ContenedorDeTestimonios />
+    </div>
   );
 };
 
