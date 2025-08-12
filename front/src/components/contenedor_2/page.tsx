399983e7f25290dc10fbc710898de844c65ab272
Ivan Ameri <dark.ranked@gmail.com>
2025-08-12 15:20:59 -0300

    cambios generales en testimonios

--- front/src/components/contenedor_2/page.tsx
+++ front/src/components/contenedor_2/page.tsx
@@ -6,7 +6,7 @@ import ContenedorDeTestimonios from "./contenedorDeTestimonios";
 
 const HeavyComponent = () => {
   return (
-    <div className="w-full max-w-[667px] h-[750px] lg:h-[592px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-4">
+    <div className="w-full max-w-[667px] h-[750px] lg:h-[900px] xl:h-[740px] bg-gray-200 rounded-lg p-4 flex flex-col items-center justify-start gap-4">
       <VideoSlider />
       <div className="mt-auto">
         <ContenedorDeTestimonios />
